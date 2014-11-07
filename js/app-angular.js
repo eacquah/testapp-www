cc9be87411f2f192b80477909085b453a6681958
Manny <manny.acquah@twentysixdigital.com>
2014-11-07 17:16:33 +0000

    Angular integration and app rewrite.

--- js/app-angular.js
+++ js/app-angular.js
@@ -1,32 +1,53 @@
-// Create module
-var app = angular.module('app', ['ngRoute']);
+// create the module and name it scotchApp
+var myApp = angular.module('myApp', ['ngRoute']);
 
-// Configure routes
-app.config(['$routeProvider', function($routeProvider) {
+// configure our routes
+myApp.config(function($routeProvider) {
     $routeProvider
-        .when('/', {
-            templateUrl: 'views/home.html',
-            controller: 'mainController'
-        })
-        .when('/news', {
-            templateUrl: 'views/news.html',
-            controller: 'newsController'
-        })
-        .when('/comics', {
-            templateUrl: 'views/comic.html',
-            controller: 'comicController'
-        })
-        .when('/toons', {
-            templateUrl: 'views/toon.html',
-            controller: 'toonController'
-        })
-        .when('/contact', {
-            templateUrl: 'views/contact.html',
-            controller: 'contactController'
-        });
-}]);
 
-app.controller('mainController', function($scope) {
-    // create message to display
-    $scope.message = 'test';
+        // route for the home page
+        .when('/', {
+            templateUrl : 'pages/home.html',
+            controller  : 'mainController'
+        })
+
+        // route for the about page
+        .when('/toons', {
+            templateUrl : 'pages/toon.html',
+            controller  : 'toonController'
+        })
+
+        // route for the about page
+        .when('/updates', {
+            templateUrl : 'pages/news.html',
+            controller  : 'newsController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        })
+
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+// create the controller and inject Angular's $scope
+myApp.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+myApp.controller('toonController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+myApp.controller('newsController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+myApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
 });
