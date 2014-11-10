f93c73de739404e2ad329c65a5cb4dabdb7658bd
manny <manny.acquah@gmail.com>
2014-11-10 22:25:13 +0000

    Fixed toon page and minor styling adjustment

--- js/app-angular.js
+++ js/app-angular.js
@@ -1,32 +1,32 @@
 // create the module and name it scotchApp
-var myApp = angular.module('myApp', ['ngRoute','slick']);
+var myApp = angular.module('myApp', ['ngRoute', 'slick']);
 
 // configure our routes
-myApp.config(function($routeProvider) {
+myApp.config(function ($routeProvider) {
     $routeProvider
 
         // route for the home page
         .when('/', {
-            templateUrl : 'pages/home.html',
-            controller  : 'mainController'
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
         })
 
         // route for the about page
         .when('/toons', {
-            templateUrl : 'pages/toon.html',
-            controller  : 'toonController'
+            templateUrl: 'pages/toon.html',
+            controller: 'toonController'
         })
 
         // route for the about page
         .when('/updates', {
-            templateUrl : 'pages/news.html',
-            controller  : 'newsController'
+            templateUrl: 'pages/news.html',
+            controller: 'newsController'
         })
 
         // route for the contact page
         .when('/contact', {
-            templateUrl : 'pages/contact.html',
-            controller  : 'contactController'
+            templateUrl: 'pages/contact.html',
+            controller: 'contactController'
         })
 
         .otherwise({
@@ -35,43 +35,42 @@ myApp.config(function($routeProvider) {
 });
 
 // create the controller and inject Angular's $scope
-myApp.controller('mainController', function($scope, $http) {
+myApp.controller('mainController', function ($scope, $http) {
 
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
 
     $http.defaults.useXDomain = true;
 
-    $http.get('http://test.local/api/comics').
-    success(function (data, status, headers, config) {
-            //alert(data[0].toSource());
-        $scope.comics = data;
-    }).
-    error(function (data, status, headers, config) {
-        // error msg
-    });
-});
-
-myApp.controller('toonController', function($scope) {
-    // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
-
-    $http.defaults.useXDomain = true;
-
-    $http.get('http://test.local/api/toons').
+    $http.get('http://lolgh.spacebarweb.com/api/comics').
         success(function (data, status, headers, config) {
-            alert(data[0].toSource());
-            $scope.latestToon = data[0];
+            //alert(data[0].toSource());
+            $scope.comics = data;
         }).
         error(function (data, status, headers, config) {
             // error msg
         });
 });
 
-myApp.controller('newsController', function($scope) {
+myApp.controller('toonController', function ($scope, $http) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+
+    $http.defaults.useXDomain = true;
+
+    $http.get('http://lolgh.spacebarweb.com/api/toons').
+        success(function (data, status, headers, config) {
+            $scope.newToon = data[0];
+        }).
+        error(function (data, status, headers, config) {
+            // error msg
+        });
+});
+
+myApp.controller('newsController', function ($scope) {
     $scope.message = 'Look! I am an about page.';
 });
 
-myApp.controller('contactController', function($scope) {
+myApp.controller('contactController', function ($scope) {
     $scope.message = 'Contact us! JK. This is just a demo.';
 });
--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 $(document).foundation();
 
 $(function() {
+    // Resize video to full screen
     $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
     // If you want to keep full screen on window resize
     $(window).resize(function(){
