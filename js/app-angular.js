9027e476ef60b534823797d3a4652679e1e61494
manny <manny.acquah@gmail.com>
2014-12-27 17:36:14 +0000

    Update to files from app

--- js/app-angular.js
+++ js/app-angular.js
@@ -54,11 +54,6 @@ myApp.directive('slickSlider', function ($http) {
                         infinite: false,
                         slidesToShow: 1,
                         slidesToScroll: 1
-                        //centerMode: true
-                        //adaptiveHeight: true
-                        //centerPadding: "60px"
-                        //fade: true
-                        //rtl: true
                     });
                 });
             });
@@ -101,7 +96,6 @@ myApp.controller('toonController', function ($scope, $http, $sce) {
             // error msg
         });
 
-
     $scope.facebookShare = function(title, img, url) {
         window.plugins.socialsharing.shareViaFacebook(title, img, url, function() {console.log('share ok')}, function(errormsg){alert(errormsg)});
     };
@@ -109,7 +103,8 @@ myApp.controller('toonController', function ($scope, $http, $sce) {
         window.plugins.socialsharing.shareViaTwitter(title, img, url);
     };
     $scope.generalShare = function(title, img, url) {
-        window.plugins.socialsharing.share(title, img, url);
+        var msg = title + ' ' + img + ' ' + url;
+        window.plugins.socialsharing.share(msg, title, img, url);
     };
 });
 
